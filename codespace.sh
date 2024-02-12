984378b2a0314b455a49d0445f5f3f6765e54b6d
Alexander <31856209+perfumescent@users.noreply.github.com>
2024-02-12 10:41:02 +0000

    cp init

---
+++ codespace.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install libsoup2.4-dev
+sudo apt-get install libxcb-shape0-dev
+sudo apt-get install libxcb-xfixes0-dev
+sudo apt install libwebkit2gtk-4.0-dev \
+    build-essential \
+    curl \
+    wget \
+    file \
+    libssl-dev \
+    libgtk-3-dev \
+    libayatana-appindicator3-dev \
+    librsvg2-dev
